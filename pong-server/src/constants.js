a6910f8d5787fea9cf9630b40a21fe74ae6acf89
larfef <larfef@proton.me>
2025-10-02 22:04:25 +0200

    Change max score

--- pong-server/src/constants.js
+++ pong-server/src/constants.js
@@ -52,7 +52,7 @@ export const GAME_CONSTANTS = {
   INITIAL_SCORE: 0,
 
   // Game termination
-  WINNING_SCORE: 11, // First to 11 points wins
+  WINNING_SCORE: 5, // First to 11 points wins
   GAME_STATES: {
     WAITING: 'waiting', // Game not started yet
     PLAYING: 'playing', // Game in progress
